a5c61e7cb1f4b91388bf2942355d1f3051c7ed48
Prajwal Kedilaya <pjkedilaya@gmail.com>
2020-09-12 22:29:28 -0700

    plot UL from AS and LSE in joint chart

---
+++ plot_UL.py
@@ -0,0 +1,49 @@
+import csv
+from dateutil import parser
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_price_dict(file_name):
+	price_dict = {}
+	with open(file_name, newline='') as file:
+		reader = csv.reader(file, delimiter=',')
+		i = 0
+		for row in reader:
+			if i == 0:
+				i+=1
+				continue
+			row_date = parser.parse(row[0])
+			price_dict[row_date] = float(row[1])
+	return price_dict
+
+UL_A = get_price_dict("Unilever_AS.csv")
+UL_L = get_price_dict("Unilever_LSE.csv")
+
+common_dates = list(UL_A.keys() & UL_L.keys())
+common_dates.sort()
+
+# print(len(UL_A))
+# print(len(UL_L))
+# print(len(common_dates))
+
+UL_A_prices = []
+UL_L_prices = []
+for common_date in common_dates:
+	UL_A_prices.append(UL_A[common_date])
+	UL_L_prices.append(UL_L[common_date])
+
+# print(len(UL_A_prices))
+# print(len(UL_L_prices))
+
+UL_A_rebased = np.array(UL_A_prices)/UL_A_prices[0]*100.0
+UL_L_rebased = np.array(UL_L_prices)/UL_L_prices[0]*100.0
+
+# y_ticks = np.arange(0, max(UL_A_prices), 2)
+# print(len(y_ticks))
+# plt.yticks(y_ticks)
+
+plt.plot(common_dates, UL_A_rebased, label="UL AS")
+plt.plot(common_dates, UL_L_rebased, label="UL LSE")
+plt.ylabel('prices rebased from start, %')
+plt.legend(loc="upper left")
+plt.show()
